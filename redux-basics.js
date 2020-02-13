05c132fd3aec86894740ee28614e7f5060e0da1f
klein20 <olexandrkleyn@gmail.com>
2020-02-13 12:31:41 +0200

    added increment

--- redux-basics.js
+++ redux-basics.js
@@ -5,15 +5,15 @@ const initialState = {
     counter: 0
 }
 
-//Reducer
+// Reducer
 const rootReducer = (state = initialState, action) => {
-    if(action.type === 'INC_COUNTER') {
+    if (action.type === 'INC_COUNTER') {
         return {
             ...state,
             counter: state.counter + 1
         };
     }
-    if(action.type === 'ADD_COUNTER') {
+    if (action.type === 'ADD_COUNTER') {
         return {
             ...state,
             counter: state.counter + action.value
@@ -22,12 +22,16 @@ const rootReducer = (state = initialState, action) => {
     return state;
 };
 
-//Store
+// Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-//Dispathing Action
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+// Dispatching Action
 store.dispatch({type: 'INC_COUNTER'});
-store.dispatch({type: 'ADD_COUNTER', value: 15});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-//Subscription
