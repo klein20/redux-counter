bed239ab36cea8d74cfe2c94c7fd2b41cdfc7b20
klein20 <olexandrkleyn@gmail.com>
2020-02-13 17:31:29 +0200

    splited on multiple reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
     counter: 0,
@@ -26,22 +26,10 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter - action.val 
             }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date() ,value: state.counter})
-            }
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1); 
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+        
     }
     return state;
 };
 
 export default reducer;
+
